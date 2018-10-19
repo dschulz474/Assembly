6bc0d211ea48df82292961cfb7e0b3012056729f
dschulz474 <dschulz474@student.glendale.edu>
2018-10-19 12:06:39 -0700

    no message

--- test.s
+++ test.s
@@ -3,18 +3,24 @@
 main:
 ldr r0, =n @load address of n into r0
 ldr r1, =sum
+ldr r2, =i
+mov r2,#0
 for:@summation for loop
-ldr r2, =i 
-add r1,r2,r0 @add r2 and r0 into r1
-mov	r2,r0
-ble for
-bgt endfor
-endfor:
+cmp r2,r0
+bgt endforloop
+add r1,r1,r2
+add r2,r2,#1
+b for
+endforloop:
+mov r0,#stdout
+mov r1,r1
+swi SWI_PrInt
 swi 0x11
 
 .data
 n:	.word 5
 sum:	.word 0
 i:	.word 0
+
 
 
