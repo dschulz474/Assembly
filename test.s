a48202f7bdb2727375dcd34e55efb05b8e011e61
dschulz474 <dschulz474@student.glendale.edu>
2018-10-22 18:45:33 -0700

    no message

--- test.s
+++ test.s
@@ -1,26 +1,30 @@
 .global _start
 
 main:
-ldr r0, =n @load address of n into r0
-ldr r1, =sum
-ldr r2, =i
-mov r2,#0
+ldr r1, =n @load address of n into r0
+ldr r2, =sum
+ldr r3, =i
+
+mov r3,#0
 for:@summation for loop
-cmp r2,r0
+cmp r3,r1
 bgt endforloop
-add r1,r1,r2
-add r2,r2,#1
+add r3,r3,#1
+add r2,r2,r3
 b for
+
 endforloop:
-mov r0,#stdout
-mov r1,r1
-swi SWI_PrInt
+mov r1,r2
+mov r0,#0
+
+swi 0x6b
 swi 0x11
 
 .data
 n:	.word 5
 sum:	.word 0
 i:	.word 0
+
 
 
 
